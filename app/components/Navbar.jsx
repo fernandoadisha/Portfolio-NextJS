33c3ec1d29bb5d18c77e580d3d1e5b55a09f26f5
Adhisha Fernando <fernandoadisha007@gmail.com>
2025-11-12 23:40:29 +0530

    Imporvement of the nav bar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,6 +5,9 @@ import React from 'react'
 const Navbar = () => {
     return (
         <>
+            <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+                <Image src={assets.header_bg_color} alt='BackgroundImage' className='w-full' />
+            </div>
             <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py flex items-center justify-between
             z-50'>
                 <a href="#top">
@@ -13,8 +16,8 @@ const Navbar = () => {
                 </a>
 
                 <ul className='hidden md:flex items-center gap-6 lg:gap-8
-                rounded-full px-12 py-3'>
-                    <li><a href="#top">Home</a></li>
+                rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
+                    <li><a className='font-Ovo' href="#top">Home</a></li>
                     <li><a href="#abput">About Me</a></li>
                     <li><a href="#services">Services</a></li>
                     <li><a href="#work">My Work</a></li>
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme: '#11001F'
+      }
+
+      ,
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"]
+      }
+    },
+  },
+
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
