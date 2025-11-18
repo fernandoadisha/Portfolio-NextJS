f2a43fc415a5a1b5b249f7c8f7f453f469f4c0b9
Adhisha Fernando <fernandoadisha007@gmail.com>
2025-11-18 20:50:35 +0530

    Completed about part and some modifictions for the nav bar

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,4 +1,4 @@
-import { assets, infoList } from '@/assets/assets'
+import { assets, infoList, toolsData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
@@ -22,8 +22,8 @@ const About = () => {
 
                     <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
                         {infoList.map(({ icon, iconDark, title, description }, index) => (
-                            <li className='border-[0.5px] border-grey-400 rounded-xl p-6 cursor-pointer hover:bg-"#de1a1aff" 
-                            hover:-translate-y-1 duration-500'
+                            <li className='border-[0.5px] border-grey-400 rounded-xl p-6 cursor-pointer hover:bg-[#fcf4ff] 
+                            hover:-translate-y-1 duration-500 hover:shadow-[#000]'
                                 key={index}>
                                 <Image src={icon} alt={title} className='w-7 mt-3' />
                                 <h3 className='my-4 font-semibold text-grey-700'>{title}</h3>
@@ -31,6 +31,21 @@ const About = () => {
                             </li>
                         ))}
                     </ul>
+
+                    <h4 className='my-6 text-grey-700 font-Ovo'>
+                        Tools I user
+                    </h4>
+
+                    <ul className='flex items-center gap-3 sm:gap-5'>
+                        {toolsData.map((tool, index) => (
+                            <li className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg
+                            cursoe-pointer hover:-translate-y-1 duration-500'
+                                key={index}>
+                                <Image src={tool} alt='Tool' className='w-5 sm:w-7' />
+                            </li>
+                        ))}
+                    </ul>
+
                 </div>
             </div>
         </div>
