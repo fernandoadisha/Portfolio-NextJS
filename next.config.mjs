698caa64965a62306c24080953c8218a9878b535
Adhisha Fernando <fernandoadisha007@gmail.com>
2026-03-16 23:53:09 +0530

    Updated some data

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   /* config options here */
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
+  images: {
+    unoptimized: true
+  },
 };
 
 export default nextConfig;
